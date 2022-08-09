5cea6af70545223ff2d9e280f6c3cbec8f0e4d3e
Mohammadkaif <mohammadkaif051197@gmail.com>
2022-08-09 11:31:18 +0530

    yes me

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ const app = express();
 const superagent = require('superagent');
 const request = require('request');
 const port = 9700;
+const cors = require('cors');
+app.use(cors());
+app.get("/",(req, res) => {
+    
+})
