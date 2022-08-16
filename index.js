db9402a7f5719d528df9c3ddf7116ff68484e534
Mohammadkaif <mohammadkaif051197@gmail.com>
2022-08-16 10:26:19 +0530

    yes me

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const superagent = require('superagent');
-const request = require('request');
+const superagent = require("superagent");
+const request = require("request");
 const port = 9700;
-const cors = require('cors');
+const cors = require("cors");
 app.use(cors());
-app.get("/",(req, res) => {
-    
-})
+app.get("/", (req, res) => {
+  res.send(
+    '<a href="https://github.com/login/oauth/authorize?client_id=9edc4c52568908cf3570">Login with github</a>'
+  );
+});
+app.get("/profile", (req, res) => {
+  res.send("ok");
+});
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
+});
