a6d47f4c14841fdf158981305d1fb3d5040aaf91
Mohammadkaif <mohammadkaif051197@gmail.com>
2022-08-16 12:42:07 +0530

    yes me

--- index.js
+++ index.js
@@ -11,7 +11,35 @@ app.get("/", (req, res) => {
   );
 });
 app.get("/profile", (req, res) => {
-  res.send("ok");
+  const code = req.query.code;
+  if (!code) {
+    res.send("Login Fail");
+  }
+
+  superagent
+    .post("https://github.com/login/oauth/access_token")
+    .send({
+      client_id: "9edc4c52568908cf3570",
+      client_secret: "64dc7110e2c25447a7641ad5ed0c3e940990bdd5",
+      code: code,
+    })
+    .set("Accept", "application/json")
+    .end((err, result) => {
+      if (err) throw err;
+      let access_token = result.body.access_token;
+      const option = {
+        uri: "https://api.github.com/user",
+        method: "GET",
+        headers: {
+          Accept: "application/json",
+          Authorization: `token ${access_token}`,
+          "User-Agent": "mycode",
+        },
+      };
+      request(option, (err, response, body) => {
+        return res.send(body);
+      });
+    });
 });
 app.listen(port, () => {
   console.log(`listening on port ${port}`);
